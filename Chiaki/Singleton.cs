511247b800fca2a8f459a54a5457f8de861609b1
Steven P <stevu@gemidyne.net>
2019-06-02 16:40:15 +0100

    Added singleton class; lazily instantiated

---
+++ Chiaki/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chiaki
+{
+    /// <summary>
+    /// Represents a singleton instance of a class
+    /// </summary>
+    /// <typeparam name="T">Type of the singleton</typeparam>
+    public class Singleton<T> where T : new()
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => new T());
+
+        /// <summary>
+        /// Gets a singleton instance of <typeparamref name="T"/>
+        /// </summary>
+        public static T Instance => _instance.Value;
+    }
+}
