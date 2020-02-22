273db65d99f2c88a9db93a0ba52286bf0b93ea8a
Steven P <stevu@gemidyne.net>
2020-02-22 17:23:57 +0000

    Extra tests

--- Chiaki.Tests/StringBuilderExtensions/AppendFormatIf.cs
+++ Chiaki.Tests/StringBuilderExtensions/AppendFormatIf.cs
@@ -69,5 +69,69 @@ namespace Chiaki.Tests.StringBuilderExtensions
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ThreeArguments_ConditionTrue()
+        {
+            // Arrange
+            var builder = new StringBuilder("this has an ");
+            var expected = "this has an appendfmt 1234 string here, 5678 and 91011";
+
+            // Act
+            builder.AppendFormatIf(condition: 1 + 1 == 2, "appendfmt {0} string here, {1} and {2}", 1234, 5678, 91011);
+
+            var actual = builder.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThreeArguments_ConditionFalse()
+        {
+            // Arrange
+            var builder = new StringBuilder("this has an ");
+            var expected = "this has an ";
+
+            // Act
+            builder.AppendFormatIf(condition: 1 + 1 == 1, "appendfmt {0} string here, {1} and {2}", 1234, 5678, 91011);
+
+            var actual = builder.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiArguments_ConditionTrue()
+        {
+            // Arrange
+            var builder = new StringBuilder("this has an ");
+            var expected = "this has an appendfmt 1234, 5678, 91011, 12394192513";
+
+            // Act
+            builder.AppendFormatIf(condition: 1 + 1 == 2, "appendfmt {0}, {1}, {2}, {3}", 1234, 5678, 91011, 12394192513);
+
+            var actual = builder.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MultiArguments_ConditionFalse()
+        {
+            // Arrange
+            var builder = new StringBuilder("this has an ");
+            var expected = "this has an ";
+
+            // Act
+            builder.AppendFormatIf(condition: 1 + 1 == 1, "appendfmt {0}, {1}, {2}, {3}", 1234, 5678, 91011, 12394192513);
+
+            var actual = builder.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
