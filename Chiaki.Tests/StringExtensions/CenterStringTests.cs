9f30d79a916156f21f5f57613c344091918877fb
Steven P <stevu@gemidyne.net>
2020-04-02 21:35:51 +0100

    More tests

---
+++ Chiaki.Tests/StringExtensions/CenterStringTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chiaki.Tests.StringExtensions
+{
+    [TestClass]
+    public class CenterStringTests
+    {
+        [TestMethod]
+        public void Scenario1()
+        {
+            // Arrange
+            string input = "test";
+            string expected = "   test   ";
+
+            // Act
+            string actual = input.CenterString(10);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThrowsArgumentNullException_WhenInputNull()
+        {
+            // Arrange
+            string input = null;
+
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(() => input.CenterString(10));
+        }
+    }
+}
---
+++ Chiaki.Tests/StringExtensions/SplitByCamelCaseTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chiaki.Tests.StringExtensions
+{
+    [TestClass]
+    public class SplitByCamelCaseTests
+    {
+        [TestMethod]
+        public void Scenario1()
+        {
+            // Arrange
+            string input = "ThisIsATest";
+            var expected = new[] { "This", "Is", "A", "Test" };
+
+            // Act
+            var actual = input.SplitByCamelCase().ToArray();
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ThrowsException_WhenInputNull()
+        {
+            // Arrange
+            string input = null;
+
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(() => input.SplitByCamelCase());
+        }
+    }
+}
